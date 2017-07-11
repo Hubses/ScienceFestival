1105222fa7426bf904888612a8b1b5dec2bfc456
vitalics <vitalicset@yandex.ru>
2017-07-11 14:18:53 +0300

    fix radio buttons inputs

--- festival/frontend/src/commonApp/radiobutton/radiobutton.component.ts
+++ festival/frontend/src/commonApp/radiobutton/radiobutton.component.ts
@@ -1,13 +1,13 @@
-import { Component,Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
     selector: 'sf-common-radio',
     templateUrl: './radiobutton.component.html'
 })
-export class RadioButtonComponent { 
-    @Input() public option: string;
-    @Input() public checked: string;   
-    
-         options:[string];
-    
+export class RadioButtonComponent {
+    // @Input() public option: string;
+    @Output() public onChenged: string;
+
+    @Input() RadioButtonOptions: [string];
+
 }
