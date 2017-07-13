e83a82a42c12e8117e89b5f06ccceb57835c188e
alexandranov <darkday97@mail.ru>
2017-07-13 12:35:53 +0300

    some modyfare

--- festival/frontend/src/commonApp/radiobutton/radiobutton.component.ts
+++ festival/frontend/src/commonApp/radiobutton/radiobutton.component.ts
@@ -7,6 +7,7 @@ import { Component,Input,Output,EventEmitter } from '@angular/core';
 export class RadioButtonComponent { 
     @Input() public option: string;
     @Input() public value: boolean;
+    @Input() public options= [];
 
     @Output() valueChange = new EventEmitter<boolean>();
     
