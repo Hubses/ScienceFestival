fcc317154763246cc06ea0bbf9271885c73436f4
DrJekill352 <ckam1997@gmail.com>
2017-07-13 17:42:31 +0300

    fixe bem

--- festival/frontend/src/app/components/news-item/news-item.component.ts
+++ festival/frontend/src/app/components/news-item/news-item.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
-    selector: 'sf-news-item',
-    templateUrl: 'news-item.component.html',
-    styleUrls: ['./news-item.component.less']
+  selector: 'sf-news-item',
+  templateUrl: 'news-item.component.html',
+  styleUrls: ['./news-item.component.less']
 })
 
 export class NewsItemComponent implements OnInit {
-    @Input() news: News;
+  @Input() news: News;
 
-    public flag: boolean = false;
+  public flag: boolean = false;
 
-    constructor() { }
+  constructor(
+    private router: Router
+  ) {}
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
+
+  onSelect(news: News) {
+    this.router.navigate(['/news', news]);
+  }
 }
