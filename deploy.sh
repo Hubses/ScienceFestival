3bf7c1021f8f5a7d9794cdc8917d8a04c7c8fdfe
Rodion Hrechukha <zzz3bra@gmail.com>
2017-07-14 23:09:26 +0300

    ci

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ([ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ])
+then
+  cd ./festival/frontend
+  npm run build-prod
+  cd ..
+  ./mvnw com.heroku.sdk:heroku-maven-plugin:1.1.4:deploy-war -DskipTests -Dheroku.appName=science-festival
+  echo 'Master published successfully.'
+fi
+
+if ([ $TRAVIS_BRANCH == "dev" ] && [ $TRAVIS_PULL_REQUEST == "false" ])
+then
+  cd ./festival/frontend
+  npm run build
+  cd ..
+  ./mvnw com.heroku.sdk:heroku-maven-plugin:1.1.4:deploy-war -DskipTests -Dheroku.appName=science-festival-dev
+  echo 'Dev published successfully.'
+fi
