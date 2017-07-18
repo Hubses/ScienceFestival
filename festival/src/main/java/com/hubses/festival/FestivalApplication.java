cafe74cd430e5f79045f7c6c30b4a57867822732
zzz3bra <zzz3bra@gmail.com>
2017-07-18 17:56:14 +0300

    Simple gzip filter and caching enabled for static assets

--- festival/src/main/java/com/hubses/festival/FestivalApplication.java
+++ festival/src/main/java/com/hubses/festival/FestivalApplication.java
@@ -1,10 +1,21 @@
 package com.hubses.festival;
 
+import com.github.ziplet.filter.compression.CompressingFilter;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import javax.servlet.Filter;
+
+@EnableAutoConfiguration
 @SpringBootApplication
 public class FestivalApplication {
+
+    @Bean
+    public Filter compressingFilter() {
+        return new CompressingFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FestivalApplication.class, args);
