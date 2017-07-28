2b222860bc1b23f25b642fdb26e0a90869a18dc1
DrJekill352 <ckam1997@gmail.com>
2017-07-28 14:07:22 +0300

    added news-create ngrx routing

--- festival/frontend/src/app/containers/news-feed/news-feed.component.ts
+++ festival/frontend/src/app/containers/news-feed/news-feed.component.ts
@@ -1,37 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { go, replace, search, show, back, forward } from '@ngrx/router-store';
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {go, replace, search, show, back, forward} from '@ngrx/router-store';
 
-import { getNewsAction, editNewsAction } from '../../../store/news';
+import {getNewsAction, editNewsAction, createNewsAction} from '../../../store/news';
 
 @Component({
-    selector: 'sf-news-feed',
-    templateUrl: 'news-feed.component.html',
-    styleUrls: ['./news-feed.component.less']
+  selector: 'sf-news-feed',
+  templateUrl: 'news-feed.component.html',
+  styleUrls: ['./news-feed.component.less']
 })
 
 export class NewsFeedComponent implements OnInit {
+  public items: sf.entities.News[];
 
+  constructor(private store: Store<sf.store.NewsStore>) {
+  }
 
-    public items: sf.entities.News[];
+  ngOnInit() {
+    this.store.select(s => s.newsReducer).subscribe(n => this.items = n.entity);
+    this.store.dispatch(getNewsAction());
+  }
 
+  viewEditClick(news: sf.entities.News) {
+    this.store.dispatch(editNewsAction(news));
+    this.store.select(s => s.newsReducer).subscribe(n => {
+      console.log(n.entity);
+    });
+    this.store.dispatch(go(`/en/admin/feed/news/edit/${news.id}`));
+  }
 
-    constructor(
-        private store: Store<sf.store.NewsStore>) {
-    }
-
-
-    ngOnInit() {
-        this.store.select(s => s.newsReducer).subscribe(n => this.items = n.entity);
-        this.store.dispatch(getNewsAction());
-    }
-    viewEditClick(news: sf.entities.News) {
-        this.store.dispatch(editNewsAction(news));
-        this.store.select(s => s.newsReducer).subscribe(n => {
-            console.log(n.entity);
-        });
-        this.store.dispatch(go(`/en/admin/feed/news/edit/${news.id}`));
-    }
+  viewCreateClick() {
+    this.store.dispatch(createNewsAction());
+    this.store.select(s => s.newsReducer).subscribe(n => {
+      this.store.dispatch(go(`/en/admin/feed/news/create/`));
+    });
+  }
 }
