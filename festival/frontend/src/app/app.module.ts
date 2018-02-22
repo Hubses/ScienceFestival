e0f074ab8b745bf0196b41f09a3446c2efab138c
vitalicset <vitalicset@yandexru>
2018-02-22 19:30:07 +0300

    implement material module

--- festival/frontend/src/app/app.module.ts
+++ festival/frontend/src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [ AppComponent ],
+  imports: [ BrowserModule, MaterialModule ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
