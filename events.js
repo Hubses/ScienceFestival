2710354870f982b0e7c82164609c96bda4d70ebd
DrJekill352 <ckam1997@gmail.com>
2018-06-03 22:47:57 +0300

    feat: init mocks

---
+++ events.js
@@ -0,0 +1,17 @@
+let events = document.querySelector('#events');
+
+events.addEventListener('click', () => {
+    window.location.replace("./events.html");
+})
+
+let newses = document.querySelector('#newses');
+
+newses.addEventListener('click', () => {
+    window.location.replace("./newses.html");
+})
+
+let title = document.querySelector('#title');
+
+title.addEventListener('click', () => {
+    window.location.replace("./index.html");
+})
