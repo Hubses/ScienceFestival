2e4ea524b007a063a93591e151a87ef213114e2f
DrJekill352 <ckam1997@gmail.com>
2018-06-05 00:39:24 +0300

    feat: add accounts

---
+++ user-account.js
@@ -0,0 +1,44 @@
+let events = document.querySelector('#events');
+
+events.addEventListener('click', () => {
+    window.location.replace("./events.html");
+})
+
+let newses = document.querySelector('#newses');
+
+newses.addEventListener('click', () => {
+    window.location.replace("./newses.html");
+})
+
+let title = document.querySelector('#title');
+
+title.addEventListener('click', () => {
+    window.location.replace("./index.html");
+})
+
+let login = document.querySelector('#login');
+
+login.addEventListener('click', () => {
+    window.location.replace("./login.html");
+});
+
+swap();
+function swap() {
+    const isLoggined = localStorage.getItem('loggined') || false;
+    if (isLoggined) {
+        document.querySelector('#login').hidden = true;
+        document.querySelector('#logout').hidden = false;
+        document.querySelector('#setting').hidden = false;
+        document.querySelector('.user_icon').hidden = false;
+    } else {
+        document.querySelector('#login').hidden = false;
+        document.querySelector('#logout').hidden = true;
+        document.querySelector('#setting').hidden = true;
+        document.querySelector('.user_icon').hidden = true;
+    }
+}
+
+document.querySelector('#logout').addEventListener('click', () => {
+    localStorage.clear();
+    swap();
+});
